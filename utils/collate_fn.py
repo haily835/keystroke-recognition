fd9a8699cd1d09ff9e4107347a0be05f47aa1412
Ly Hai <lyquochai835@gmail.com>
2024-08-15 01:20:55 -0400

    Update collate_fn.py

--- utils/collate_fn.py
+++ utils/collate_fn.py
@@ -31,15 +31,14 @@ def mix_up_fn(data):
 
 
 f_transforms = torchvision.transforms.v2.Compose([
-    torchvision.transforms.v2.ColorJitter(brightness=.5, contrast=.5),
-    torchvision.transforms.v2.RandomZoomOut(side_range=(1, 1.2))
+    torchvision.transforms.v2.ColorJitter(brightness=.5, contrast=.5)
 ])
 
 def color_zoom_fn(data):
     videos, labels = zip(*data)
     videos = torch.stack(videos)
     labels = torch.tensor(labels).long()
-    
+
     videos = f_transforms(videos)
     videos = videos.permute(0, 2, 1, 3, 4)
     videos = (videos / 255.0).float()
