e5392c18532ac1579669458dece1c83170956830
Ly Hai <lyquochai835@gmail.com>
2024-08-31 21:08:22 -0400

    add hyperformer

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,6 @@ from .resnet import resnet101, resnet50
 from .ctrgcn.model import CTRGCN
 from .infogcn.model import InfoGCN
 from .skateformer import SkateFormer
+from .hyperformer import Model as HyperFormer
 
-__all__ = ['resnet101', 'resnet50', 'CTRGCN', 'InfoGCN', 'SkateFormer']
+__all__ = ['resnet101', 'resnet50', 'CTRGCN', 'InfoGCN', 'SkateFormer', 'HyperFormer']
