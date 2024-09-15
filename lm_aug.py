84ad770858904aa4f6f7745d50c6e968d62de260
Ly Hai <lyquochai835@gmail.com>
2024-09-15 12:56:38 -0400

    delete unused files

--- lm_aug.py
+++
@@ -1,9 +0,0 @@
-from lightning.pytorch.cli import LightningCLI
-from lightning_utils.lm_augmented import LMModule
-from lightning_utils.lm_module import KeyClf
-
-cli = LightningCLI(
-    model_class=KeyClf,
-    datamodule_class=LMModule,
-    save_config_callback=None
-)
--- utils/import_by_classpath.py
+++
@@ -1,5 +0,0 @@
-import importlib
-
-def import_by_modulepath(module_path):
-    module = importlib.import_from_string(module_path)
-    return module
