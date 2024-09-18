f2cbbac8daf312592317428bd1b6ee39a8b2ca67
Ly Hai <lyquochai835@gmail.com>
2024-09-18 18:46:23 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,4 +4,5 @@ from .infogcn.model import InfoGCN
 from .skateformer import SkateFormer
 from .hyperformer import Model as HyperFormer
 from .hdgcn.HDGCN import Model as HDGCN
-__all__ = ['resnet10', 'resnet101', 'resnet50', 'CTRGCN', 'InfoGCN', 'SkateFormer', 'HyperFormer', 'HDGCN']
+from .stgcn.st_gcn import Model as STGCN
+__all__ = ['resnet10', 'resnet101', 'resnet50', 'CTRGCN', 'InfoGCN', 'SkateFormer', 'HyperFormer', 'HDGCN', 'STGCN']
