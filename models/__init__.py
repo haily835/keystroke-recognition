690a3dc8d07c03bba91d3fdf32dc82802098eb79
Ly Hai <lyquochai835@gmail.com>
2024-09-18 12:59:01 -0400

    Add hdgcn

--- models/__init__.py
+++ models/__init__.py
@@ -3,5 +3,5 @@ from .ctrgcn.model import CTRGCN
 from .infogcn.model import InfoGCN
 from .skateformer import SkateFormer
 from .hyperformer import Model as HyperFormer
-
-__all__ = ['resnet10', 'resnet101', 'resnet50', 'CTRGCN', 'InfoGCN', 'SkateFormer', 'HyperFormer']
+from .hdgcn.HDGCN import Model as HDGCN
+__all__ = ['resnet10', 'resnet101', 'resnet50', 'CTRGCN', 'InfoGCN', 'SkateFormer', 'HyperFormer', 'HDGCN']
