404159705f48a927516bbb5522526070536bb22c
Ly Hai <lyquochai835@gmail.com>
2025-01-05 22:19:48 -0500

    ignore numbers

--- lightning_utils/lm_dataset.py
+++ lightning_utils/lm_dataset.py
@@ -13,7 +13,7 @@ from mediapipe.framework.formats import landmark_pb2
 
 
 clf_id2label = ['comma', 'period', 'backspace', 'space', 'shift',
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                # '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                 'a', 'b', 'c', 'd',
                 'e', 'f', 'g', 'h',
                 'i', 'j', 'k', 'l',
