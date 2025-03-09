aceccea0ad774e0b6cac9a06f23e8902d3b90c1f
Hai Ly <53532432+haily835@users.noreply.github.com>
2025-03-09 10:39:49 -0400

    Update dataset.py

--- lightning_utils/dataset.py
+++ lightning_utils/dataset.py
@@ -39,12 +39,15 @@ import glob
 #                 'u', 'v', 'w', 'x',
 #                 'y', 'z']
 
-clf_id2label = ["a", "b", "c", "d", "e", 
+clf_id2label = [
+"a", "b", "c", "d", "e", 
 "f", "g", "h", "i", "j", 
 "k", "l", "m", "n", "o", 
 "p", "q", "r", "s", "t", 
-"u", "v", "w", "x", "y", 
-"z", "comma", "period", "space", "backspace"]
+"u", "v", "w", "x", "y", "z", 
+"comma", "period", "space", "backspace",
+"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+]
 
 clf_label2id = {label: idx for idx, label in enumerate(clf_id2label)}
 
