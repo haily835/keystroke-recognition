23df177a02995aff59fa16a39e6c55e1353053d2
Hai Ly <53532432+haily835@users.noreply.github.com>
2025-03-10 17:50:40 -0400

    Update MyModel.py

--- models/MyModel.py
+++ models/MyModel.py
@@ -4,12 +4,12 @@ from torch_geometric.nn import MessagePassing, HypergraphConv
 from einops import rearrange
 import math
 from torchsummary import summary
-HI = [[ 0,  1,  2,  3,  4,  0,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
+HI = torch.tensor([[ 0,  1,  2,  3,  4,  0,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
           0, 17, 18, 19, 20, 21, 22, 23, 24, 25, 22, 26, 27, 28, 29, 30, 31, 32,
          33, 34, 35, 36, 37, 21, 38, 39, 40, 41],
         [ 0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  2,  2,  2,  2,  3,  3,  3,  3,
           4,  4,  4,  4,  4,  5,  5,  5,  5,  5,  6,  6,  6,  6,  6,  7,  7,  7,
-          7,  8,  8,  8,  8,  9,  9,  9,  9,  9]]
+          7,  8,  8,  8,  8,  9,  9,  9,  9,  9]])
 # Hypergraph convolution with temperal attention.
 class HCTA(nn.Module):
     def __init__(self, in_channels, n_joints, out_channels, hyperedge_index):
