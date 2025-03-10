525cf3bea74f537219e9fce5842cf809d5d1abdc
Hai Ly <53532432+haily835@users.noreply.github.com>
2025-03-10 19:55:46 -0400

    Update MyModel.py

--- models/MyModel.py
+++ models/MyModel.py
@@ -27,7 +27,8 @@ class HCTA(nn.Module):
         [ 0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  2,  2,  2,  2,  3,  3,  3,  3,
           4,  4,  4,  4,  4,  5,  5,  5,  5,  5,  6,  6,  6,  6,  6,  7,  7,  7,
           7,  8,  8,  8,  8,  9,  9,  9,  9,  9]])
-        
+        print("HI", HI.get_device())
+        print("x", x.get_device())
         T, N, F = x.size()
         x = torch.stack([self.hc(g, HI) for g in x])
         
