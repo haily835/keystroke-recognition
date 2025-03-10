c5adbefe936b4ea2aea574825783a908c95ecfaa
Hai Ly <53532432+haily835@users.noreply.github.com>
2025-03-10 19:57:36 -0400

    Update MyModel.py

--- models/MyModel.py
+++ models/MyModel.py
@@ -27,6 +27,9 @@ class HCTA(nn.Module):
         [ 0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  2,  2,  2,  2,  3,  3,  3,  3,
           4,  4,  4,  4,  4,  5,  5,  5,  5,  5,  6,  6,  6,  6,  6,  7,  7,  7,
           7,  8,  8,  8,  8,  9,  9,  9,  9,  9]])
+        
+        HI = HI.to(device='cuda')
+
         print("HI", HI.get_device())
         print("x", x.get_device())
         T, N, F = x.size()
