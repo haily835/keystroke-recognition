0682aa307485b092e194a024e523ecca53643782
Hai Ly <53532432+haily835@users.noreply.github.com>
2025-03-11 01:45:25 -0400

    clean up

--- train.py
+++ train.py
@@ -1,7 +1,5 @@
 from lightning.pytorch.cli import LightningCLI
 from lightning_utils.lm_module import LmKeyClf
-from lightning_utils.module import KeyClf
-from lightning_utils.datamodule import KeyStreamModule
 from lightning_utils.lm_datamodule import LmKeyStreamModule
 
 cli = LightningCLI()
