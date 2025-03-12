6cec0288a49e54292edd44849b7eeb3a44e6eca7
Hai Ly <53532432+haily835@users.noreply.github.com>
2025-03-11 22:05:14 -0400

    Update MyModel.py

--- models/MyModel.py
+++ models/MyModel.py
@@ -289,7 +289,7 @@ class TCN_HC_unit(nn.Module):
 
 class MyModel(nn.Module):
     def __init__(self, num_class=40, num_point=21, num_person=2, in_channels=3,
-                 drop_out=0, adaptive=True):
+                 drop_out=0):
         super(MyModel, self).__init__()
 
         self.num_class = num_class
@@ -297,16 +297,16 @@ class MyModel(nn.Module):
         self.data_bn = nn.BatchNorm1d(num_person * in_channels * num_point)
 
         base_channel = 64
-        self.l1 = TCN_HC_unit(in_channels, base_channel, residual=False, adaptive=adaptive)
-        self.l2 = TCN_HC_unit(base_channel, base_channel, adaptive=adaptive)
-        self.l3 = TCN_HC_unit(base_channel, base_channel, adaptive=adaptive)
-        self.l4 = TCN_HC_unit(base_channel, base_channel, adaptive=adaptive)
-        self.l5 = TCN_HC_unit(base_channel, base_channel*2, stride=2, adaptive=adaptive)
-        self.l6 = TCN_HC_unit(base_channel*2, base_channel*2, adaptive=adaptive)
-        self.l7 = TCN_HC_unit(base_channel*2, base_channel*2, adaptive=adaptive)
-        self.l8 = TCN_HC_unit(base_channel*2, base_channel*4, stride=2, adaptive=adaptive)
-        self.l9 = TCN_HC_unit(base_channel*4, base_channel*4, adaptive=adaptive)
-        self.l10 = TCN_HC_unit(base_channel*4, base_channel*4, adaptive=adaptive)
+        self.l1 = TCN_HC_unit(in_channels, base_channel, residual=False)
+        self.l2 = TCN_HC_unit(base_channel, base_channel)
+        self.l3 = TCN_HC_unit(base_channel, base_channel)
+        self.l4 = TCN_HC_unit(base_channel, base_channel)
+        self.l5 = TCN_HC_unit(base_channel, base_channel*2, stride=2)
+        self.l6 = TCN_HC_unit(base_channel*2, base_channel*2)
+        self.l7 = TCN_HC_unit(base_channel*2, base_channel*2)
+        self.l8 = TCN_HC_unit(base_channel*2, base_channel*4, stride=2)
+        self.l9 = TCN_HC_unit(base_channel*4, base_channel*4)
+        self.l10 = TCN_HC_unit(base_channel*4, base_channel*4)
         self.fc = nn.Linear(base_channel*4, num_class)
         nn.init.normal_(self.fc.weight, 0, math.sqrt(2. / num_class))
         bn_init(self.data_bn, 1)
